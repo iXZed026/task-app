7ad6e30bee60f78d2d64b947af462da0f416135e
danyal <danyal.titanka24@gmail.com>
2024-05-01 16:10:57 +0330

    users component style complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
 import React, { useState } from 'react';
 import "./App.css";
 import Header from './component/Header/Header';
-
-// const usersJsonData = require('./json/Users.json');
+import Users from './component/Users/Users';
 
 const App: React.FC = () => {
-
-  // const [Users, setUsers] = useState<[]>();
-
-
-
   return (
     <div className="app">
       <Header />
+      <Users />
     </div>
   )
 }
--- src/component/Users/Users.tsx
+++ src/component/Users/Users.tsx
@@ -1,12 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import "./Users.css";
+import axios from 'axios';
+import { FaSearch } from "react-icons/fa";
+
+interface User {
+    id: number,
+    name: string,
+    lastName: string,
+    nationalCode: string,
+}
+
 
 const Users: React.FC = () => {
+
+    const usersJsonData = require('../../json/Users.json');
+
+    const [users, setUsers] = useState<any>(usersJsonData);
+
+
+
     return (
         <div className="users">
             <div className="users-container">
                 <div className="users-flex">
-
+                    <div className="search">
+                        <FaSearch id="icon-style" />
+                    </div>
+                    <div className="users-table">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>نام</th>
+                                    <th>نام خانوادگی</th>
+                                    <th>کد ملی</th>
+                                    <th>عملیات</th>
+                                </tr>
+                            </thead>
+                            <tbody id="table-body">
+                                {
+                                    users.map((user:User,key:number)=>(
+                                        <tr key={user.id}>
+                                    <td id='border'>{user.name}</td>
+                                    <td>{user.lastName}</td>
+                                    <td>{user.nationalCode}</td>
+                                    <td id='radius'>d m l g</td>
+                                </tr>
+                                    ))
+                                }
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
