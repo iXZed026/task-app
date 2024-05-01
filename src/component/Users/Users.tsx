627ee641732de09bfcb772a0e5664f88909419e2
danyal <danyal.titanka24@gmail.com>
2024-05-01 12:39:14 +0330

    create users component

---
+++ src/component/Users/Users.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Users.css";
+
+const Users: React.FC = () => {
+    return (
+        <div className="users">
+            <div className="users-container">
+                <div className="users-flex">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Users
